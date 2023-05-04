569636f44636c68fc11ce1fbf603c7f7e446aa8b
JustInTimez <just.oost@gmail.com>
2023-05-04 16:49:35 +0200

    Adds JSDoc for store state

--- static/js/model/store.js
+++ static/js/model/store.js
@@ -1,7 +1,20 @@
+/**
+ * Initial state for the Tally App
+ * @typedef {Object} InitialState
+ * @property {number} count - The current count of the Tally App
+ */
+
+/** @type {InitialState} */
 const initialState = {
   count: 0,
 };
 
+/**
+ * Creates a Redux-inspired store to manage the state of the Tally App
+ * @param {InitialState} initialState - The initial state of the Tally App
+ * @param {Function} reducer - The reducer function that will handle state updates
+ * @returns {Object} An object containing the getState, dispatch, and subscribe methods
+ */
 function createStore(initialState, reducer) {
   let state = initialState;
   const listeners = [];
